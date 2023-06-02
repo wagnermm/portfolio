5f59025a2c380395487bb33bd9303be65c543431
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-06-02 15:27:12 -0300

    adiciona new project

--- portifolio-react/src/Components/Projects/Projects.jsx
+++ portifolio-react/src/Components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import './Projects.css';
 import BarberLogo from '../Images/logo.jpg';
-import Calculator from '../Images/calculator.png'
-import Cake from '../Images/cake.gif'
+import Calculator from '../Images/calculator.png';
+import ConfectLogo from '../Images/confectonery.png';
 
 
 const Projects=()=> {
@@ -10,13 +10,13 @@ const Projects=()=> {
             <h1>Meus projetos</h1>
             <div className="project-cards" id="projects">
                 <div className="project">
-                    <a href="http://" target="_blank" rel="noopener noreferrer"><img src={BarberLogo} alt="logo" /></a>
+                    <a href="https://wagnermm.github.io/barbershop/" target="_blank" rel="noopener noreferrer"><img src={BarberLogo} alt="logo" /></a>
                 </div>
                 <div className="project">
                     <a href="http://" target="_blank" rel="noopener noreferrer"><img src={Calculator} alt="logo" /></a>
                 </div>
                 <div className="project">
-                    <a href=" https://wagnermm.github.io/confectionery/" target="_blank" rel="noopener noreferrer"><img src={Cake} alt="cake"/></a>
+                    <a href=" https://wagnermm.github.io/confectionery/" target="_blank" rel="noopener noreferrer"><img src={ConfectLogo} alt="confectonery"/></a>
                 </div>
                 <div className="project">
 
