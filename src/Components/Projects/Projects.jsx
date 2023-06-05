f04e8256c0308f5027e55b7c1622db076a8a2d03
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-06-05 09:16:27 -0300

    alteracao em projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,35 +1,35 @@
 import './Projects.css';
-import BarberLogo from '../Images/logo.jpg';
-import Calculator from '../Images/calculator.png';
-import ConfectLogo from '../Images/confectonery.png';
-import Mario from '../Images/mario.gif'
 
 
 const Projects=()=> {
     return(
-        <div className="main-project">
-            <h1>Meus projetos</h1>
-            <div className="project-cards" id="projects">
-                <div className="project">
-                    <a href="https://wagnermm.github.io/barbershop/" target="blank" rel="noopener noreferrer"><img src={BarberLogo} alt="logo" /></a>
-                </div>
-                <div className="project">
-                    <a href="http://" target="_blank" rel="noopener noreferrer"><img src={Calculator} alt="logo" /></a>
-                </div>
-                <div className="project">
-                    <a href=" https://wagnermm.github.io/confectionery/" target="blank" rel="noopener noreferrer"><img src={ConfectLogo} alt="confectonery"/></a>
-                </div>
-                <div className="project">
-                    <a href="https://wagnermm.github.io/mario-game/" target="blank"><img src={Mario} alt="mario" /></a>
-                </div>
-                <div className="project">
+        <div className="project-container">
+            <div className="project-title" id='projects'>
+                <h1>Projetos</h1>
+            </div>
+            <div className="project-body">
+                <div className="project-content">
 
                 </div>
-                <div className="project">
+                <div className="project-content">
 
+                </div>
+                <div className="project-content">
+
+                </div>
+                <div className="project-content">
+
+                </div>
+                <div className="project-content">
+
+                </div>
+                <div className="project-content">
+                    
                 </div>
             </div>
-            <div className="top"><a href="#top">Voltar ao topo</a></div>
+            <div className="top">
+                <a href="#top">Voltar ao topo</a>
+            </div>
         </div>
     )
 }
