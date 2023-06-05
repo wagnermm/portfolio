caf1c17a4b0cae982c4751c0da656089355ecc84
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-06-05 10:59:03 -0300

    adiciona projetos

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,8 @@
 import './Projects.css';
+import LogoConfectonery from '../Images/confectonery.png';
+import Mario from '../Images/mario.gif';
+import LogoBarberShop from '../Images/logo.jpg';
+import CalculatorImg from '../Images/calculator.png';
 
 
 const Projects=()=> {
@@ -9,16 +13,28 @@ const Projects=()=> {
             </div>
             <div className="project-body">
                 <div className="project-content">
-
+                    <div className="project-link">
+                        <h3>Confeitaria</h3>
+                        <a href="https://wagnermm.github.io/confectionery/" target='blank'><img src={LogoConfectonery} alt="logo-confeitaria" /></a>
+                    </div>
                 </div>
                 <div className="project-content">
-
+                    <div className="project-link">
+                        <h3>Mario Jump</h3>
+                        <a href="https://wagnermm.github.io/mario-game/" target='blank'><img src={Mario} alt="mario" /></a>
+                    </div>
                 </div>
                 <div className="project-content">
-
+                    <div className="project-link">
+                        <h3>BarberShop</h3>
+                        <a href="https://wagnermm.github.io/barbershop/" target='blank'><img src={LogoBarberShop} alt="logo-barbearia" /></a>
+                    </div>
                 </div>
                 <div className="project-content">
-
+                    <div className="project-link">
+                        <h3>Calculator</h3>
+                        <a href="" target='blank'><img src={CalculatorImg} alt="calculadora" /></a>
+                    </div>
                 </div>
                 <div className="project-content">
 
