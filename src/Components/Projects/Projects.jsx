d64ede7d4c191a06e5f8f6448dd96399f17661b3
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-07-21 11:53:50 -0300

    alterando projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,12 +2,14 @@ import './Projects.css';
 import LogoConfectonery from '../Images/confectonery.png';
 import Mario from '../Images/mario.gif';
 import LogoBarberShop from '../Images/logo.jpg';
-import CalculatorImg from '../Images/calculator.png';
+import CalculatorImg from '../Images/calculator.gif';
 import TelaLogin from '../Images/telalogin.png';
 import PHlogo from '../Images/ph.png';
 import Imc from '../Images/imc.png';
 import WebStore from '../Images/webstore.jpg';
 import NetWeb from '../Images/netweb.png';
+import { FaBootstrap, FaSass } from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io';
 
 
 const Projects=()=> {
@@ -18,57 +20,116 @@ const Projects=()=> {
             </div>
             <div className="project-body">
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Confeitaria</h3>
-                        <a href="https://wagnermm.github.io/confectionery/" target='blank'><img src={LogoConfectonery} alt="logo-confeitaria" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={LogoConfectonery} alt="logo-confeitaria"/>
+                    </div>
+                    <div className="project-description">
+                        <p>Um dos primeiros projetos criados. Uma confeitaria desenvolvida com bootstrap, explorando conceitos de modal, slide carousel e um design de cores que faz referência a seus produtos.</p>
+                        <div className="project-tecnology">
+                            <div className="bootstrap-icon">
+                                <FaBootstrap/>
+                            </div>
+                            <div className="sass-icon">
+                                <FaSass/>
+                            </div>
+                            <div className="js-icon">
+                                <IoLogoJavascript/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/confectionery/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Mario Jump</h3>
-                        <a href="https://wagnermm.github.io/mario-game/" target='blank'><img src={Mario} alt="mario" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={Mario} alt="mario" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/mario-game/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>BarberShop</h3>
-                        <a href="https://wagnermm.github.io/barbershop/" target='blank'><img src={LogoBarberShop} alt="logo-barbearia" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={LogoBarberShop} alt="logo-barbearia" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/barbershop/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Calculator</h3>
-                        <a href="https://wagnermm.github.io/calculator-js/" target='blank'><img src={CalculatorImg} alt="calculadora" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={CalculatorImg} alt="calculadora" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/calculator-js/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                <div className="project-link">
+                    <div className="project-name">
                         <h3>Tela de Login</h3>
-                        <a href="https://wagnermm.github.io/form-css/" target='blank'><img src={TelaLogin} alt="Telalogin" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={TelaLogin} alt="Telalogin" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/form-css/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Escritório de Advocacia</h3>
-                        <a href="https://wagnermm.github.io/law-firm/" target='blank'><img src={PHlogo} alt="phLogo" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={PHlogo} alt="phLogo" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/law-firm/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Calculadora IMC</h3>
-                        <a href=" https://wagnermm.github.io/IMC/" target='blank'><img src={Imc} alt="imc" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={Imc} alt="imc" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href=" https://wagnermm.github.io/IMC/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Colares artesanais</h3>
-                        <a href="https://wagnermm.github.io/web-store/" target='blank'><img src={WebStore} alt="loja" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={WebStore} alt="loja" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/web-store/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
                 <div className="project-content">
-                    <div className="project-link">
+                    <div className="project-name">
                         <h3>Net Web</h3>
-                        <a href="https://wagnermm.github.io/netweb/" target='blank'><img src={NetWeb} alt="net web" /></a>
+                    </div>
+                    <div className="project-image">
+                        <img src={NetWeb} alt="net web" />
+                    </div>
+                    <div className="project-link">
+                        <button><a href="https://wagnermm.github.io/netweb/" target='blank'>Ver Projeto</a></button>
                     </div>
                 </div>
             </div>
